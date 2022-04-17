e0698a6e1068eab608c7fcebe551e9ad9f1099b6
Novitasari-12 <novit8537@gmail.com>
2022-04-17 10:01:28 +0700

    projek1

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+//$angka = "1";
+//$variabel=["hallo","dunia"];
+//foreach($variabel as $value){
+//   echo $angka++ ."." . $value;
+//  echo "<br/>";
+// }
+
+
+// rumus luas 
+// Function  volumetrapesium($r1, $r2, $t){
+//     return $hasiltrapesium = 0.5 * ($r1+$r2) * $t;
+// }
+// echo volumetrapesium(12,12,3);
+
+
+// menghitung panjang karakter
+
+function Mencaripanjangkarakter($karakter){ 
+    return strlen($karakter);
+}
+echo Mencaripanjangkarakter("ini Saya");
+
+
+
+
